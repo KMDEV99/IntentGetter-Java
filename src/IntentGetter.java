26bd028272ab3b0b7023d6ce59352c02951ce8a4
KMDEV99 <matkonrad99@gmail.com>
2020-11-17 23:39:01 +0100

    removed redundant dependency

--- src/IntentGetter.java
+++ src/IntentGetter.java
@@ -4,9 +4,11 @@ import java.util.regex.Pattern;
 
 class IntentGetter {
     Pattern pattern;
+    Map<String, String> classification_map;
 
     public IntentGetter(Map<String, String> classification_map) {
         StringBuilder pattern_builder = new StringBuilder();
+        this.classification_map = classification_map;
 
         for (String key : classification_map.keySet())
             pattern_builder.append("|" + "\\b").append(key).append("\\b");
@@ -14,7 +16,7 @@ class IntentGetter {
         pattern = Pattern.compile(pattern_builder.substring(1));
     }
 
-    public String get_intent(Map<String, String> classification_map, String text) {
+    public String get_intent(String text) {
         String intent = "brak";
         Matcher matcher = pattern.matcher(text);
 
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
+
         Map<String, String> classification_map = TreeMapManager.get_filled_classification();
         IntentGetter IG = new IntentGetter(classification_map);
-        System.out.println(IG.get_intent(classification_map, sentence));
+
+        System.out.println(IG.get_intent(sentence));
     }
 }
